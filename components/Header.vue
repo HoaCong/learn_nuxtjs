457042b8c6fc42313ed8adbf493a446e31c055a8
dev-hoale <leconghoa11a1@gmail.com>
2023-06-28 08:22:31 +0700

    update layout with priveflex

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,20 @@
 <template>
   <Menubar>
     <template v-slot:start>
-      <NuxtLink to="/" class="logo">MENT</NuxtLink>
+      <NuxtLink to="/" class="text-5xl text-900 font-bold no-underline"
+        >MENT</NuxtLink
+      >
     </template>
     <template v-slot:end>
       <NuxtLink
         to="/about"
-        class="text-900"
+        class="text-900 hover:text-blue-500"
         v-bind:class="{ 'text-blue-500': isLinkActive('/about') }"
         >About</NuxtLink
       >
       <NuxtLink
         to="/todo"
-        class="text-900 ml-2"
+        class="text-900 ml-2 hover:text-blue-500"
         v-bind:class="{ 'text-blue-500': isLinkActive('/todo') }"
         >Todo</NuxtLink
       >
@@ -31,27 +33,5 @@ export default defineComponent({
 </script>
 
 <style>
-.logo {
-  font-size: 25px;
-  font-weight: bold;
-  color: #000;
-  text-decoration: none;
-}
-
-.logo:hover {
-  background-color: unset;
-}
-
-.header-right a {
-  margin-left: 16px;
-}
-.header-right a:hover {
-  text-decoration: underline;
-  color: rgb(5, 99, 250);
-}
-
-.header-right a.active {
-  text-decoration: underline;
-  color: rgb(5, 99, 250);
-}
+/*  */
 </style>
