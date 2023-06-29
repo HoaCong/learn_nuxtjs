2381c9ee58c04bd085727f0a786694b50b89aec6
dev-hoale <leconghoa11a1@gmail.com>
2023-06-29 23:11:30 +0700

    write test COM inputButton

---
+++ components/_inputButton.spec.ts
@@ -0,0 +1,42 @@
+import { shallowMount } from "@vue/test-utils";
+import InputButton from "./inputButton.vue";
+import InputText from "primevue/inputtext";
+import { describe, expect, it, beforeEach, afterEach, vi } from "vitest";
+
+describe("InputButton", () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = shallowMount(InputButton, {
+      propsData: {
+        modelValue: "",
+        placeholder: "Enter username",
+        label: "Submit",
+        class: "custom-class",
+        handleActions: vi.fn(),
+        ariaDescribedby: "description",
+      },
+    });
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it("renders the component correctly", () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("emits an update event when input value changes", async () => {
+    const input = wrapper.findComponent(InputText);
+    await input.setValue("Hòa Lê");
+    expect(wrapper.emitted("update:modelValue")).toBeTruthy();
+    expect(wrapper.emitted("update:modelValue")[0][0]).toBe("Hòa Lê");
+  });
+
+  it("calls handleActions method when the button is clicked", () => {
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.vm.handleActions).toHaveBeenCalled();
+  });
+});
